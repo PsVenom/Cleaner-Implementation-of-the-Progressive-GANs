5f6e86790e524bd818999181a7085c8b3eaef9cd
Atul-Ranjan12 <33323817+Atul-Ranjan12@users.noreply.github.com>
2022-03-28 20:15:31 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+## TODO: 1) Complete Implementation of the Generator and Discriminator block:
+## TODO:        a)Implement the mixing variables, scaled weights, and Minibatch Standard Deviation
+## TODO: 2) Test the Discriminator and Generator networks (Make sure that the discriminator is a complete mirror of the generator)
+## TODO: 3) Implement the Training Loop and the training step.
+## TODO: 4) Test it on the CIFAR Dataset and compare the results to the paper
+## TODO: 5) Test on the art Dataset
